1376c18da6e2c68b02b143bf07be6165a3de91d8
Richard Nuno <me@richardnuno.com>
2013-09-01 12:52:34 -0400

    Added default mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,9 @@ PongLords::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
+  # Default host for mailer
+  config.action_mailer.default_url_options = { :host => ENV["PL_MAIL_HOST"] }
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
